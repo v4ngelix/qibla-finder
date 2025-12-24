9040337381eaa5c0bc719ce83407476914942691
v4ngelix <andi.angel@boheemia.ee>
2025-12-24 22:15:34 +0200

    Add instruction

---
+++ src/Instruction/Instruction.tsx
@@ -0,0 +1,11 @@
+import './style.css';
+
+function Instruction() {
+  return (
+    <div className="qibla__instruction">
+      Click on the map to find Qibla
+    </div>
+  )
+}
+
+export default Instruction;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import { render } from 'preact';
 import Map from './Map';
+import Instruction from './Instruction';
 
 import './style.css';
 
 export function App() {
 	return (
-		<Map />
+		<>
+			<Map />
+			<Instruction />
+		</>
 	);
 }
 
