b4bd8fe589a19a54eef8524b53154604f31421e3
v4ngelix <andi.angel@boheemia.ee>
2025-12-24 21:04:50 +0200

    Refactor layers usage

--- src/Map/Map.tsx
+++ src/Map/Map.tsx
@@ -5,7 +5,7 @@ import { MapboxOverlay } from '@deck.gl/mapbox';
 import { ScenegraphLayer } from '@deck.gl/mesh-layers';
 import { LineLayer } from '@deck.gl/layers';
 import getSessionToken, { SessionTokenRequestResponse } from "./getSessionToken";
-import { kaabaCoordinates, primaryGreenRGB } from "./constants";
+import { defaultZoom, kaabaCoordinates, kaabaCoordinates3D, primaryGreenRGB } from "./constants";
 import kaabaModelUrl from "../assets/holy kaaba/scene.gltf?url";
 import getBasemapStyle from "./getBasemapStyle";
 
@@ -13,20 +13,24 @@ function Map() {
   const mapContainerRef = useRef<HTMLDivElement>(null);
   const [ clickedPosition, setClickedPosition ] = useState<[number, number] | null>(null);
   const [ map, setMap ] = useState<MaplibreMap | undefined>(undefined);
+  const [ zoom, setZoom ] = useState<number>(defaultZoom);
   const deckOverlayRef = useRef<MapboxOverlay | null>(null);
 
-  const kaaba3D = (
-    new ScenegraphLayer({
-      id: 'kaaba-model',
-      data: [{ position: kaabaCoordinates }],
-      scenegraph: kaabaModelUrl,
-      getPosition: (d: any) => d.position,
-      getOrientation: (_d: any) => [0, 32, 90],
-      sizeMinPixels: 20,
-      sizeScale: 1,
-      _lighting: 'pbr'
-    })
-  );
+  const kaaba3D = useMemo(() => {
+    console.log('rerender?')
+    return (
+
+      new ScenegraphLayer({
+        id: 'kaaba-model',
+        data: [{ position: kaabaCoordinates3D }],
+        scenegraph: kaabaModelUrl,
+        getPosition: (d: any) => [d.position[0], d.position[1], 0],
+        getOrientation: (_d: any) => [0, 32, 90],
+        sizeScale: 10,
+        _lighting: 'pbr'
+      })
+    );
+  }, [ zoom ]);
 
   useEffect((): void => {
     if (map === undefined) {
@@ -39,7 +43,7 @@ function Map() {
           container: mapContainerRef.current,
           style: getBasemapStyle(response.session),
           center: kaabaCoordinates,
-          zoom: 8,
+          zoom: defaultZoom,
           maxZoom: 18,
         });
 
@@ -55,13 +59,19 @@ function Map() {
           layers: [
             kaaba3D
           ],
-          interleaved: false
+          interleaved: true
         });
 
         newMap.addControl(deckOverlay as any);
         newMap.on('click', (e) => {
           setClickedPosition([e.lngLat.lng, e.lngLat.lat]);
         });
+        newMap.on('zoom', () => {
+          const roundedZoom: number = Math.round(newMap.getZoom());
+          if (roundedZoom === zoom) {
+            setZoom(roundedZoom);
+          }
+        })
 
         deckOverlayRef.current = deckOverlay;
         setMap(newMap);
@@ -90,7 +100,7 @@ function Map() {
             source: clickedPosition,
             target: kaabaCoordinates
           }],
-          getWidth: 2,
+          getWidth: 4,
           getSourcePosition: (d: LineData): [ number, number, number] => [d.source[0], d.source[1], 0],
           getTargetPosition: (d: LineData): [ number, number, number] => [d.target[0], d.target[1], 0],
           getColor: primaryGreenRGB,
@@ -99,7 +109,7 @@ function Map() {
     }
 
     deckOverlayRef.current.setProps({ layers });
-  }, [ clickedPosition ]);
+  }, [ clickedPosition, zoom ]);
 
   return (
     <div id="map" ref={ mapContainerRef } />
