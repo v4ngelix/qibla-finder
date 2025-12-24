7e2e0fa18f52faf9106d1f1e61e4476476badcd3
v4ngelix <andi.angel@boheemia.ee>
2025-12-24 21:01:23 +0200

    Add separate coordinate for placing the Kaaba's model

--- src/Map/constants.ts
+++ src/Map/constants.ts
@@ -1,4 +1,11 @@
 export const googleMapsAPIKey: string = 'AIzaSyD4ZvE5Mkdc2hQ-QY07TnZF6Mclfibzjxg';
 export const kaabaCoordinates: [number, number] = [ 39.82619985500611, 21.422506610479076 ];
+
+/**
+ * Differend coordinate for the modal,
+ * as the model's center point is somewhat offset
+ * and the actual coordinate will misplace the model.
+ */
+export const kaabaCoordinates3D: [number, number] = [ 39.82485, 21.4217 ];
 export const primaryGreenRGB: [ number, number, number ] = [ 71, 133, 85 ];
 export const primaryGreenHEX: string = '#478555';
