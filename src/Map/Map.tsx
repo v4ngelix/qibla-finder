d83e1c3f41978c75267d5182b6d34400173abbfe
v4ngelix <andi.angel@boheemia.ee>
2025-12-24 20:22:19 +0200

    Add initial layers

--- src/Map/Map.tsx
+++ src/Map/Map.tsx
@@ -1,96 +1,105 @@
 import { Map as MaplibreMap, NavigationControl } from "maplibre-gl";
-import { useEffect, useRef, useState } from "preact/hooks";
+import { useEffect, useMemo, useRef, useState } from "preact/hooks";
 import "maplibre-gl/dist/maplibre-gl.css";
 import { MapboxOverlay } from '@deck.gl/mapbox';
 import { ScenegraphLayer } from '@deck.gl/mesh-layers';
+import { LineLayer } from '@deck.gl/layers';
 import getSessionToken, { SessionTokenRequestResponse } from "./getSessionToken";
-import { googleMapsAPIKey, kaabaCoordinates } from "./constants";
+import { kaabaCoordinates, primaryGreenRGB } from "./constants";
 import kaabaModelUrl from "../assets/holy kaaba/scene.gltf?url";
+import getBasemapStyle from "./getBasemapStyle";
 
 function Map() {
   const mapContainerRef = useRef<HTMLDivElement>(null);
-  const [ map, setMap ] = useState<MaplibreMap>();
+  const [ clickedPosition, setClickedPosition ] = useState<[number, number] | null>(null);
+  const [ map, setMap ] = useState<MaplibreMap | undefined>(undefined);
+  const deckOverlayRef = useRef<MapboxOverlay | null>(null);
 
-  useEffect(() => {
-    console.log('effect')
-    getSessionToken()
-      .then((
-        response: SessionTokenRequestResponse
-      ) => {
-      const newMap = new MaplibreMap({
-        container: mapContainerRef.current,
-        style: {
-          version: 8,
-          projection: {
-            type: 'globe'
-          },
-          sources: {
-            osm: {
-              type: "raster",
-              tiles: [ "https://tile.openstreetmap.org/{z}/{x}/{y}.png" ],
-              tileSize: 256,
-              attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            },
-            googleHybrid: {
-              type: "raster",
-              tiles: [`https://tile.googleapis.com/v1/2dtiles/{z}/{x}/{y}?session=${ response.session }&key=${ googleMapsAPIKey }`],
-              tileSize: 256
-            }
-          },
+  const kaaba3D = (
+    new ScenegraphLayer({
+      id: 'kaaba-model',
+      data: [{ position: kaabaCoordinates }],
+      scenegraph: kaabaModelUrl,
+      getPosition: (d: any) => d.position,
+      getOrientation: (_d: any) => [0, 32, 90],
+      sizeMinPixels: 20,
+      sizeScale: 1,
+      _lighting: 'pbr'
+    })
+  );
+
+  useEffect((): void => {
+    if (map === undefined) {
+      console.log('effect')
+      getSessionToken()
+        .then((
+          response: SessionTokenRequestResponse
+        ): void => {
+        const newMap = new MaplibreMap({
+          container: mapContainerRef.current,
+          style: getBasemapStyle(response.session),
+          center: kaabaCoordinates,
+          zoom: 8,
+          maxZoom: 18,
+        });
+
+        newMap.addControl(
+          new NavigationControl({
+            visualizePitch: true,
+            showZoom: true,
+            showCompass: true
+          })
+        );
+
+        const deckOverlay = new MapboxOverlay({
           layers: [
-            {
-              id: "osm",
-              type: "raster",
-              source: "osm",
-              minzoom: 0,
-              maxzoom: 19
-            },
-            {
-              id: "googleHybrid",
-              type: "raster",
-              source: "googleHybrid",
-              minzoom: 14,
-              maxzoom: 19
-            },
-          ]
-        },
-        center: kaabaCoordinates,
-        zoom: 18,
-        maxZoom: 18,
-        pitchWithRotate: false,
-        dragRotate: false,
-        touchZoomRotate: false
+            kaaba3D
+          ],
+          interleaved: false
+        });
+
+        newMap.addControl(deckOverlay as any);
+        newMap.on('click', (e) => {
+          setClickedPosition([e.lngLat.lng, e.lngLat.lat]);
+        });
+
+        deckOverlayRef.current = deckOverlay;
+        setMap(newMap);
       });
-
-      newMap.addControl(
-        new NavigationControl({
-          visualizePitch: true,
-          showZoom: true,
-          showCompass: true
-        })
-      );
-
-      // Add deck.gl ScenegraphLayer for Kaaba 3D model
-      const deckOverlay = new MapboxOverlay({
-        layers: [
-          new ScenegraphLayer({
-            id: 'kaaba-model',
-            data: [{ position: [...kaabaCoordinates, 0] }],
-            scenegraph: kaabaModelUrl,
-            getPosition: (d: any) => d.position,
-            getOrientation: (d: any) => [0, 0, 90],
-            sizeScale: 1,
-            _lighting: 'pbr'
-          } as any)
-        ]
-      });
-
-      newMap.addControl(deckOverlay as any);
-
-      setMap(newMap);
-    });
-
+    }
   }, [ mapContainerRef ]);
+
+  useEffect((): void => {
+    if (!deckOverlayRef.current) return;
+
+    const layers: any[] = [
+      kaaba3D
+    ];
+
+    if (clickedPosition) {
+      console.log("clickedPosition", clickedPosition);
+      type LineData = {
+        source: [number, number];
+        target: [number, number];
+      };
+
+      layers.push([
+        new LineLayer<LineData>({
+          id: 'qibla-direction-line',
+          data: [{
+            source: clickedPosition,
+            target: kaabaCoordinates
+          }],
+          getWidth: 2,
+          getSourcePosition: (d: LineData): [ number, number, number] => [d.source[0], d.source[1], 0],
+          getTargetPosition: (d: LineData): [ number, number, number] => [d.target[0], d.target[1], 0],
+          getColor: primaryGreenRGB,
+        })
+      ]);
+    }
+
+    deckOverlayRef.current.setProps({ layers });
+  }, [ clickedPosition ]);
 
   return (
     <div id="map" ref={ mapContainerRef } />
