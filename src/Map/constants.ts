6e5104703bab19a974bfaa52e5064ad88602a54e
v4ngelix <andi.angel@boheemia.ee>
2025-12-24 20:03:00 +0200

    Define primary green color

--- src/Map/constants.ts
+++ src/Map/constants.ts
@@ -1,2 +1,4 @@
 export const googleMapsAPIKey: string = 'AIzaSyD4ZvE5Mkdc2hQ-QY07TnZF6Mclfibzjxg';
-export const kaabaCoordinates: [number, number] = [ 39.825635, 21.42220 ];
+export const kaabaCoordinates: [number, number] = [ 39.82619985500611, 21.422506610479076 ];
+export const primaryGreenRGB: [ number, number, number ] = [ 71, 133, 85 ];
+export const primaryGreenHEX: string = '#478555';
