8fb0cd71f45638b6cde5afb15d69643a22c84c44
v4ngelix <andi.angel@boheemia.ee>
2026-01-10 09:46:53 +0200

    Fix grammar

--- src/Instruction/Instruction.tsx
+++ src/Instruction/Instruction.tsx
@@ -4,7 +4,7 @@ function Instruction() {
   return (
     <div className="qibla__instruction-container">
       <div className="qibla__instruction">
-        Click on the map to find Qibla
+        Click on the map to find the Qibla
       </div>
     </div>
   )
