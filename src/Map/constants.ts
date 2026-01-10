3a0e1423bcaf1177c292734f52d0a14c3f5a47d2
v4ngelix <andi.angel@boheemia.ee>
2026-01-10 09:47:24 +0200

    Revert "Use Google maps instead of google raster and Maplibre to get access to satellite tiles"
    This reverts commit fd56540da39a3ff494dbc54bbfb5475e6c2aa195.

--- src/Map/constants.ts
+++ src/Map/constants.ts
@@ -1,3 +1,4 @@
+export const googleMapsAPIKey: string = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 export const defaultZoom = 16;
 export const kaabaCoordinates: [number, number] = [ 39.82619985500611, 21.422506610479076 ];
 export const primaryGreenRGB: [ number, number, number ] = [ 71, 133, 85 ];
