310bd802d320165a583b41c2d431dc3903b90e22
v4ngelix <andi.angel@boheemia.ee>
2026-01-10 11:34:06 +0200

    Only use Google if token is good

--- src/Map/getBasemapStyle.ts
+++ src/Map/getBasemapStyle.ts
@@ -5,13 +5,28 @@ import basestyle from './style.json';
 const getBasemapStyle = (
   sessionToken: string
 ): StyleSpecification => {
-  console.log('KANA', sessionToken, googleMapsAPIKey)
+
 
   let basemapStyleString: string = JSON.stringify(basestyle);
   basemapStyleString = basemapStyleString.replace('{key}', mapTileAPIKey);
-  basemapStyleString = basemapStyleString.replace('{google-maps-session-token}', sessionToken);
-  basemapStyleString = basemapStyleString.replace('{google-maps-api-key}', googleMapsAPIKey);
   basemapStyleString = basemapStyleString.replace('{map-background-color}', primaryGreenHEX);
+
+  if (sessionToken) {
+    basemapStyleString = basemapStyleString.replace('{google-maps-session-token}', sessionToken);
+    basemapStyleString = basemapStyleString.replace('{google-maps-api-key}', googleMapsAPIKey);
+    const basemapStyleObject = JSON.parse(basemapStyleString);
+    const layers = basemapStyleObject.layers;
+    basemapStyleObject.layers = [
+      ...layers,
+      {
+        "id": "google-hybrid",
+        "type": "raster",
+        "source": "google-hybrid",
+        "minzoom": 14
+      }
+    ];
+    return basemapStyleObject;
+  }
 
   return JSON.parse(basemapStyleString)
 };
