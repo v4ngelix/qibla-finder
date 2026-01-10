7777cdf6e03a32bc0f5c509cdf268dbf8de9ab7c
v4ngelix <andi.angel@boheemia.ee>
2026-01-10 11:06:13 +0200

    Modify Kaaba position

--- src/Map/constants.ts
+++ src/Map/constants.ts
@@ -2,7 +2,7 @@ import { RGB } from "./types";
 
 export const googleMapsAPIKey: string = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
 export const defaultZoom = 16;
-export const kaabaCoordinates: [number, number] = [ 39.82619985500611, 21.422506610479076 ];
+export const kaabaCoordinates: [number, number] = [ 39.8261818877661, 21.422519417868614 ];
 export const whiteRGB: RGB = [ 255, 255, 255 ];
 export const primaryGreenRGB: RGB = [ 71, 133, 85 ];
 export const primaryGreenHEX: string = '#478555';
