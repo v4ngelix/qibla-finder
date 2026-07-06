9238dd1e310d2db1e980f556bfb11f317e980e1c
Andi Angel <andi.angel@boheemia.ee>
2026-07-06 16:19:20 +0300

    Remove unused variables

--- src/Map/constants.ts
+++ src/Map/constants.ts
@@ -1,10 +1,8 @@
 import { RGB } from "./types";
 
 export const googleMapsAPIKey: string = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
-export const mapTileAPIKey: string = import.meta.env.VITE_MAPTILER_API_KEY;
 export const defaultZoom = 16;
 export const kaabaCoordinates: [number, number] = [ 39.8261818877661, 21.422519417868614 ];
 export const whiteRGB: RGB = [ 255, 255, 255 ];
 export const primaryGreenRGB: RGB = [ 71, 133, 85 ];
-export const primaryGreenHEX: string = '#478555';
 export const backgroundOpacity: number = .125;
