356a7704d3b48bc120b8f7b8f97fd4507d53d7d1
Andi Angel <andi.angel@boheemia.ee>
2026-07-06 18:42:32 +0300

    Fix displaying of the satellite tiles layer

--- src/Map/getGoogleTileLayer.ts
+++ src/Map/getGoogleTileLayer.ts
@@ -17,11 +17,11 @@ const getGoogleTileLayer = (
     getFillColor: [255, 255, 255]
   }),
   new TileLayer({
-    id: 'google-satellite',
-    data: `https://tile.googleapis.com/v1/2dtiles/{z}/{x}/{y}?session=${satelliteSessionToken}&key=${googleMapsAPIKey}`,
-    minZoom: 10,
-    maxZoom: 22,
-    tileSize: 512,
+    id: 'google-roadmap',
+    data: `https://tile.googleapis.com/v1/2dtiles/{z}/{x}/{y}?session=${roadmapSessionToken}&key=${googleMapsAPIKey}`,
+    minZoom: 0,
+    maxZoom: 10,
+    tileSize: 256,
     renderSubLayers: (props: any) => {
       const { west, south, east, north } = props.tile.bbox;
 
@@ -33,11 +33,11 @@ const getGoogleTileLayer = (
     }
   }),
   new TileLayer({
-    id: 'google-roadmap',
-    data: `https://tile.googleapis.com/v1/2dtiles/{z}/{x}/{y}?session=${roadmapSessionToken}&key=${googleMapsAPIKey}`,
-    minZoom: 0,
-    maxZoom: 10,
-    tileSize: 512,
+    id: 'google-satellite',
+    data: `https://tile.googleapis.com/v1/2dtiles/{z}/{x}/{y}?session=${satelliteSessionToken}&key=${googleMapsAPIKey}`,
+    minZoom: 10,
+    maxZoom: 22,
+    tileSize: 256,
     renderSubLayers: (props: any) => {
       const { west, south, east, north } = props.tile.bbox;
 
