6ff480b321c345dce96515401d5d1ea26f836cda
Andi Angel <andi.angel@boheemia.ee>
2026-07-12 14:08:42 +0300

    Add initial sky

--- src/Map/getGoogleStyle.ts
+++ src/Map/getGoogleStyle.ts
@@ -5,10 +5,30 @@ import type { SessionTokens } from './getSessionToken';
 const getTileURL = (sessionToken: string): string =>
 	`https://tile.googleapis.com/v1/2dtiles/{z}/{x}/{y}?session=${sessionToken}&key=${googleMapsAPIKey}`;
 
+// TODO: Add nice looking sky configuration
 const getGoogleStyle = (sessionTokens: SessionTokens): StyleSpecification => ({
 	version: 8,
 	projection: { type: 'globe' },
 	glyphs: 'https://fonts.openmaptiles.org/{fontstack}/{range}.pbf',
+	sky: {
+		"sky-color": "#199EF3",
+		"sky-horizon-blend": 0.5,
+		"horizon-color": "#ffffff",
+		"horizon-fog-blend": 0.5,
+		"fog-color": "#0000ff",
+		"fog-ground-blend": 0.5,
+		"atmosphere-blend": [
+			"interpolate",
+			["linear"],
+			["zoom"],
+			0,
+			1,
+			10,
+			1,
+			12,
+			0
+		]
+	},
 	sources: {
 		'google-roadmap': {
 			type: 'raster',
